07b9526d6c87d7fb8ddb93e0ff24562aa7b70915
Betrouni <99085759+Betrouni@users.noreply.github.com>
2023-11-06 20:02:38 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("CRUD Application");
+});
+
+let items = ["User", "Ready Player One", "Zboubati"]; // Ceci va agir comme une base de données en mémoire
+
+// Create (POST)
+app.post("/items", (req, res) => {
+  const item = req.body;
+  items.push(item);
+  res.send(`Item with the name ${item.name} added to the database!`);
+});
+
+// Read (GET)
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+// Update (PUT)
+app.put("/items/:name", (req, res) => {
+  const name = req.params.name;
+  const item = req.body;
+  items = items.map((i) => {
+    if (i.name === name) {
+      return item;
+    }
+    return i;
+  });
+  res.send(`Item with the name ${name} updated in the database!`);
+});
+
+// Delete (DELETE)
+app.delete("/items/:name", (req, res) => {
+  const name = req.params.name;
+  items = items.filter((i) => i.name !== name);
+  res.send(`Item with the name ${name} removed from the database!`);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
